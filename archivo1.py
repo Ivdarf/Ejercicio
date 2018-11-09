fb35b47c14d0e172c1d1aab4f027f5b23ecd9ee1
Ivan D <44857719+Ivdarf@users.noreply.github.com>
2018-11-09 13:03:04 -0500

    Definicion de funcion multiplicar para matrices

--- archivo1.py
+++ archivo1.py
@@ -1,5 +1,19 @@
 import numpy as np
 
+def multiplicar(a,b):
+  if len(a[0,:]!=len(b[:,0]):
+         print "Operacion no se puede realizar"
+  else:
+         res=np.zeros((len(a[:,0]),len(b[0,:])))
+         for i in range(0,len(a[:,o])):
+              for j in range(0,len(b[o,:])):
+                  for k in range(0,len(a[o,:])):
+                      res[i][j]+=a[i][k]*b[k][j]
+         return res
+  return 0
+         
+         
 a=np.array(([3,2,1],[4,2,3],[5,2,1]))
 e=np.array(([-24,18,5],[20,-15,-4],[-5,4,1]))
+multiplicar(a,e)
 
